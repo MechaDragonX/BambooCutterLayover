a6f8cc3a38670f13d57b17a363ce9df7ed0219b1
MechaDragonX <theofficalrv@gmail.com>
2021-04-03 19:54:14 -0700

    Fix types for input elements to properly grab and change text in fields

--- docs/index.ts
+++ docs/index.ts
@@ -1,10 +1,9 @@
-const input: Element = document.querySelector('#inputField');
-const processedText: Element = document.querySelector('#processedText');
+const input: HTMLInputElement = (<HTMLInputElement>document.querySelector('#inputField'));
+const processedText: HTMLInputElement = (<HTMLInputElement>document.querySelector('#processedText'));
 
 input.addEventListener('change', processText);
 
 function processText() {
-    // @ts-ignore
     processedText.value = input.value;
 }
 
